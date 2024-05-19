783014751a3047b4264d7eb14d45f21f3e20a21e
John Roice Aldeza <jroicealdeza@gmail.com>
2024-05-19 21:23:36 +0800

    chore: Update VitePWA configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        globPatterns: ["**/*.{ts,js,css,html,tsx,ico,png,svg,jpg}"],
-      },
-      devOptions: {
-        enabled: true
-      },
-      includeAssets: ["favicon.ico", "apple-touch.png"],
-      manifest: {
-        name: "COPD Early Detection",
-        short_name: "COPD Early Detection",
-        description: "COPD Early Detection",
-        theme_color: "#FAF4D3",
-        icons: [
-          {
-            src: "192-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "512-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "512-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "512-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
+    
   ],
 });
